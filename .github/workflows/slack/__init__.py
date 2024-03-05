0069b183582f63498a6596744c7340d7d17727a2
e.martirosian <e.martirosian@revolut.com>
2024-03-05 03:09:57 +0300

    asdasd

--- .github/workflows/slack/__init__.py
+++ .github/workflows/slack/__init__.py
@@ -4,18 +4,17 @@ from slack.errors import SlackApiError
 from os import getenv
 
 if __name__ == '__main__':
-    os = argv[1]
-    file_path = argv[2]
-    client = WebClient(token=getenv('SLACK_TOKEN'))
-    channel_id = getenv('SLACK_CHANNEL')
-    message = "New " + os + " build"
-
     try:
+        os = argv[1]
+        file_path = argv[2]
+        client = WebClient(token=getenv('SLACK_TOKEN'))
+        channel_id = getenv('SLACK_CHANNEL')
+        message = "New " + os + " build"
         response = client.files_upload(
             channels=channel_id,
             initial_comment=message,
             file=file_path
         )
         print("Message sent successfully:", response["ts"])
-    except SlackApiError as e:
+    except Exception as e:
         print("Error sending message:", e.response['error'])
